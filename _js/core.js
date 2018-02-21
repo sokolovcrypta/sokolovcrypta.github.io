c98d44b1a594c0d9082cdae80fda469945ce2a9e
unknown <naraxiss@gmail.com>
2018-02-22 01:27:17 +0300

    speakers

--- _js/core.js
+++ _js/core.js
@@ -3,5 +3,22 @@ $(document).ready(function() {
         $('body').toggleClass('open-menu')
     })
 
-   
+    $('.speaker__more').on('click', function(){
+    	if($(this).html() !== 'Скрыть'){
+    		$(this).closest('.speaker').find('.speaker__hide').slideDown(300);
+	    	$(this).html('Скрыть');
+    	} else {
+    		$(this).closest('.speaker').find('.speaker__hide').slideUp(300);
+	    	$(this).html('Подробнее');
+    	}
+    	
+    })
+
+
+    $(".menu--nav .menu__item").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).find('a').attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 });
